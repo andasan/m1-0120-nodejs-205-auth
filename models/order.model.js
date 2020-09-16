8012c7abc5e6fc675425a7d23506fc1324b35598
Francois Polo <andasan@gmail.com>
2020-09-16 10:33:37 -0700

    removeFromCart

--- models/order.model.js
+++ models/order.model.js
@@ -7,7 +7,18 @@ const orderSchema = new Schema({
             product: { type: Object, required: true },
             quantity: { type: Number, required: true }
         }
-    ]
+    ],
+    user: {
+        name: {
+            type: String,
+            required: true
+        },
+        userId: {
+            type: Schema.Types.ObjectId,
+            required: true,
+            ref: 'User'
+        }
+    }
 });
 
 module.exports = mongoose.model('Order', orderSchema);
