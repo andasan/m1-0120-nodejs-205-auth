9a0f0d5ebe3d51d3202ff095ef71a980fdd074fd
Francois Polo <andasan@gmail.com>
2020-09-23 09:19:03 -0700

    router labels

--- routes/shop.route.js
+++ routes/shop.route.js
@@ -8,20 +8,34 @@ const shopController = require('../controllers/shop.controller');
 // @access  Public
 router.get('/', shopController.getProducts);
 
-//get one product
+// @route   GET /products
+// @desc    Get a certain product by id
+// @access  Public
 router.get('/products/:productId', shopController.getOneProduct);
 
-//go to cart page
+// @route   GET /cart
+// @desc    Get products added in cart by a user
+// @access  Public
 router.get('/cart', shopController.getCart);
 
-//post a product on cart ---or add to cart
+// @route   POST /cart
+// @desc    For users to add a product in a cart
+// @access  Public
 router.post('/cart', shopController.postCart);
 
-// // delete a product from cart
+// @route   POST /cart
+// @desc    Removes a product from the cart
+// @access  Public
 router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
+// @route   POST /orders
+// @desc    Consolidate all products in cart to orders collection
+// @access  Public
 router.get('/orders', shopController.getOrders);
 
+// @route   GET /orders
+// @desc    Get all products in orders collection by user id
+// @access  Public
 router.post('/create-order', shopController.postOrder);
 
 // router.get('/checkout', shopController.getCheckOut);
