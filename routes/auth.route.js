f530d61b8ced78343da58fd8fc4709522ef253eb
Francois Polo <andasan@gmail.com>
2020-09-23 22:57:54 -0700

    delete unnecessary code/files

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,10 +2,19 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth.controller');
 
+// @route   GET /login
+// @desc    Login page
+// @access  Public
 router.get('/login', authController.getLogin);
 
+// @route   POST /login
+// @desc    Authenticate a user
+// @access  Public
 router.post('/login', authController.postLogin);
 
+// @route   POST /logout
+// @desc    Un-authenticate a user
+// @access  Public
 router.post('/logout', authController.postLogout);
 
 module.exports = router;
