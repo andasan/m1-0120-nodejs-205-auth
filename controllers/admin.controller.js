ae07a6ff0d1749ed9aabe18a2584ad7a6cc784dd
Francois Polo <andasan@gmail.com>
2020-09-30 12:08:02 -0700

    delete file upon product delete

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,7 +1,8 @@
 const Product = require('../models/product.model');
 const {validationResult} = require('express-validator');
-
 const mongoose = require('mongoose');
+
+const fileHelper = require('../util/file');
 
 exports.getAddProduct = (req,res,next) => {
     res.render('admin/edit-product', {
@@ -78,9 +79,11 @@ exports.getEditProduct = (req,res,next) => {
 exports.postEditProduct = (req,res,next) => {
     const prodId = req.body.productId;
     const updatedTitle = req.body.title;
-    const updatedImageUrl = req.body.imageUrl;
+    const updatedImage = req.file;
     const updatedDesc = req.body.description;
     const updatedPrice = req.body.price;
+
+    console.log(updatedImage);
 
     const errors = validationResult(req);
     if(!errors.isEmpty()){
@@ -98,9 +101,13 @@ exports.postEditProduct = (req,res,next) => {
             if(product.userId.toString() !== req.user._id.toString()){
                 return res.redirect('/');
             }
-            product.title = updatedTitle,
-            product.imageUrl = updatedImageUrl,
-            product.description = updatedDesc,
+            product.title = updatedTitle
+            // product.image = updatedImage
+            if(updatedImage){
+                fileHelper.deleteFile(product.image);
+                product.image = updatedImage.path;
+            }
+            product.description = updatedDesc
             product.price = updatedPrice
             return product.save().then(() => {
                 console.log('Updated Product');
@@ -133,7 +140,14 @@ exports.getProducts = (req,res,next) => {
 exports.postDeleteProduct = (req,res,next) => {
     const prodId = req.body.productId;
     Product
-        .deleteOne({_id: prodId, userId: req.user._id})
+        .findById(prodId)
+        .then(product => {
+            if(!product){
+                return next(new Error('Product not found.'))
+            }
+            fileHelper.deleteFile(product.image);
+            return Product.deleteOne({_id: prodId, userId: req.user._id})
+        })
         .then(() => {
             console.log('Deleted Product');
             res.redirect('/admin/products');
