98f70fedbf0847cf5063f7f2743a87758fc212a8
Francois Polo <andasan@gmail.com>
2020-09-30 11:30:38 -0700

    pdfkit done

--- controllers/shop.controller.js
+++ controllers/shop.controller.js
@@ -248,7 +248,7 @@ exports.getInvoice = (req, res, next) => {
 
             const pdfDoc = new PDFDocument();
             res.setHeader('Content-Type', 'application/pdf');
-            res.setHeader('Content-Disposition', 'inline; filename=" '+invoiceName +' " ');
+            res.setHeader('Content-Disposition', 'inline; filename=" ' + invoiceName + ' " ');
 
             pdfDoc.pipe(fs.createWriteStream(invoicePath));
             pdfDoc.pipe(res);
@@ -256,6 +256,16 @@ exports.getInvoice = (req, res, next) => {
             pdfDoc.fontSize(26).text('Invoice', {
                 underline: true
             });
+            pdfDoc.text('-----------------------------');
+            let totalPrice = 0;
+            order.products.forEach(prod => {
+                totalPrice += prod.quantity * prod.product.price;
+                pdfDoc.fontSize(14).text(
+                    prod.product.title + ' - ' + prod.quantity + ' x ' + '$' + prod.product.price
+                );
+            });
+            pdfDoc.text('-----------------------------');
+            pdfDoc.fontSize(20).text('Total Price: $' + totalPrice);
 
             pdfDoc.end();
 
